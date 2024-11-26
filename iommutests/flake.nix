4133d531f9fcd999f62ee72838fd9c5b054a9618
Joel Granados <joel.granados@joelgranados.com>
2024-11-26 16:34:03 +0100

    Add development envs for iommutests and kernel projects
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

---
+++ iommutests/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "iommutests dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    libvfn.url = "github:Joelgranados/libvfn/cc68647b8a4d95d3cb101e036a5662dbb0f696d5";
+  };
+
+  outputs = { self, nixpkgs, libvfn, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          libvfn.packages.${system}.default
+          meson
+          ninja
+          git
+          gnumake
+          pkg-config
+          cmake
+          clang-tools
+          nodePackages.pyright
+          man-pages
+          python311Packages.pytest
+          python311Packages.pyudev
+          git-filter-repo
+          clang-tools
+          man-pages
+        ];
+        hardeningDisable = ["fortify"];
+
+        shellHook = ''
+          export SHELL=$(command -v zsh)
+          exec $SHELL
+        '';
+      };
+    };
+}
