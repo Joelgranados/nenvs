e9c4ccdc06571bdf17b63a817a85777e7e3d6532
Joel Granados <joel.granados@joelgranados.com>
2024-11-28 13:44:33 +0100

    Update kdevops flake to use kernel flake outputs
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- kdevops/flake.nix
+++ kdevops/flake.nix
@@ -3,55 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    env_shell.url = "github:Joelgranados/nix_envs?dir=env_shell";
+    env_kernel.url = "github:Joelgranados/nix_envs?dir=kernel";
+
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, env_shell, env_kernel, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       system = "x86_64-linux";
     in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          gnumake
-          bison
-          flex
-          ncurses
-          (pkgs.python3.withPackages (ppkgs: [
-            ppkgs.alabaster
-            ppkgs.sphinx
-            ppkgs.pyyaml
-          ]))
-          pahole
-          elfutils
-          ccache
-          bc
-          gdb
-          openssl
-          gcc14
-          cpio
-          kmod
-          zlib
-          clang-tools
-          coccinelle
-          man-pages
-          clang-tools
-          git-filter-repo
-          git
-          pkg-config
-        ];
-        #hardeningDisable = ["fortify"];
+
+        shellPkgs = with pkgs;
+        [
+        ] ++ env_kernel.devShells.${system}.default.shellPkgs ;
+        packages = self.devShells.${system}.default.shellPkgs;
 
         shellHook = ''
-          export PATH=${pkgs.ccache}/bin:$PATH
-          export CC="ccache gcc"
-          export CXX="ccache g++"
-          alias make="make CC='"$CC"'"
-          export CCACHE_DIR=/home/joel/.cache/.ccache
-          mkdir -p $CCACHE_DIR
-          echo "ccache configured with directory $CCACHE_DIR"
-          export SHELL=$(command -v zsh)
-          exec $SHELL
-        '';
+        ''
+        + env_kernel.devShells.${system}.default.shellHook ;
       };
     };
 }
