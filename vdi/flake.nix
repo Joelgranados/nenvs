56433a32d0922934e603dddf787b64cb8251d5b3
Joel Granados <joel.granados@joelgranados.com>
2024-12-03 09:41:00 +0100

    Enable flake switching in VDI
    This enables the creation of a new nixos generation with the following
    command:
    `sudo nixos-rebuild switch --impure --flake ./vdi`
    We probably will not use it like this as we have the specialization. But
    it is nice to have
    
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- vdi/flake.nix
+++ vdi/flake.nix
@@ -9,23 +9,49 @@
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       system = "x86_64-linux";
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs;
-        [
-          virt-manager
-          spice-vdagent
-          spice-gtk
-          usbredir
-          qemu_kvm
-          libvirt
-        ];
+    in
+  {
+    nixosConfigurations = {
+      hobbes = nixpkgs.lib.nixosSystem {
+        modules = [
+          # FIXME: Avoid hardcoding this!
+          /etc/nixos/configuration.nix
+          {
+            programs.virt-manager.enable = true;
 
-        shellHook = ''
-          ${./vdictrl.sh} -n win11 -a start
-          trap '${./vdictrl.sh} -n win11 -a stop' EXIT
-        ''
-        ;
+            virtualisation.libvirtd.enable = true;
+            virtualisation.spiceUSBRedirection.enable = true;
+
+            users.groups.libvirtd.members = [ "joel" ];
+            environment.systemPackages = with pkgs; [
+              virt-manager
+              spice-vdagent
+              spice-gtk
+              usbredir
+              qemu
+              libvirt
+            ];
+          }
+        ];
       };
     };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs;
+      [
+        virt-manager
+        spice-vdagent
+        spice-gtk
+        usbredir
+        qemu_kvm
+        libvirt
+      ];
+
+      shellHook = ''
+        ${./vdictrl.sh} -n win11 -a start
+        trap '${./vdictrl.sh} -n win11 -a stop' EXIT
+      ''
+      ;
+    };
+  };
 }
