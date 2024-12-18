03673a0a5c0ba960bd60b9aa5e1948d8084fad3a
Joel Granados <joel.granados@joelgranados.com>
2024-12-18 13:59:17 +0100

    Add mailmisc flake
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

---
+++ mailmisc/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "mailmisc flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    env_shell.url = "github:Joelgranados/nix_envs?dir=env_shell";
+  };
+
+  outputs = { self, nixpkgs, env_shell, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        shellPkgs = with pkgs;
+        [
+          shiv
+          gnumake
+        ];
+        packages = self.devShells.${system}.default.shellPkgs;
+
+        shellHook = ''
+          export _prompt_sorin_prefix="%F{green}(M6C)"
+        ''
+        + env_shell.devShells.${system}.default.shellHook
+        ;
+      };
+    };
+}
