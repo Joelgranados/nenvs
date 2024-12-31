14606ab843db313f1cf2c60e7088c8bcdea4e060
Joel Granados <joel.granados@joelgranados.com>
2024-12-31 15:20:09 +0100

    Add vmctl environment
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

---
+++ vmctl/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "vmctl flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    env_shell.url = "github:Joelgranados/nix_envs?dir=env_shell";
+  };
+
+  outputs = { self, nixpkgs, env_shell, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        shellPkgs = with pkgs;
+        [
+          cdrtools
+        ];
+        packages = self.devShells.${system}.default.shellPkgs;
+
+        shellHook = ''
+          export _prompt_sorin_prefix="%F{green}(V3L)"
+        ''
+        + env_shell.devShells.${system}.default.shellHook
+        ;
+      };
+    };
+}
