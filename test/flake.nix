3b7108c80814967bcfcd8970c9c3cc6d9e514272
Joel Granados <joel.granados@joelgranados.com>
2025-01-21 11:07:19 +0100

    Add a test flake
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

---
+++ test/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "test flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        shellPkgs = with pkgs;
+        [
+          gnumake
+        ];
+        packages = self.devShells.${system}.default.shellPkgs;
+      };
+    };
+}
