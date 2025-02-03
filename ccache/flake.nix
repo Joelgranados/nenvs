5cca056e3df7753757f5df09ba75b3070bd93674
Joel Granados <joel.granados@joelgranados.com>
2025-02-03 14:07:13 +0100

    ccache: remove the message when we enter the ccache shell
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- ccache/flake.nix
+++ ccache/flake.nix
@@ -22,7 +22,7 @@
           alias make="make CC='"$CC"'"
           export CCACHE_DIR=/home/joel/.cache/.ccache
           mkdir -p $CCACHE_DIR
-          echo "ccache configured with directory $CCACHE_DIR"
+          #echo "ccache configured with directory $CCACHE_DIR"
         '';
       };
     };
