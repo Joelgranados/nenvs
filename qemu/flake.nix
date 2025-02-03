d835c11e0914445573570532d95c77bbd87eaf0d
Joel Granados <joel.granados@joelgranados.com>
2025-02-03 10:51:56 +0100

    qemu: add remote compilation
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- qemu/flake.nix
+++ qemu/flake.nix
@@ -7,9 +7,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     env_shell.url = "github:Joelgranados/nix_envs?dir=env_shell";
     env_ccache.url = "github:Joelgranados/nix_envs?dir=ccache";
+    krc.url = "github:Joelgranados/nix_envs?dir=krc";
   };
 
-  outputs = { self, nixpkgs, env_shell, env_ccache, ... }:
+  outputs = { self, nixpkgs, env_shell, env_ccache, krc, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       system = "x86_64-linux";
@@ -25,7 +26,11 @@
           lz4
           libslirp
           clang-tools
-        ] ++ env_ccache.devShells.${system}.default.shellPkgs ;
+
+          krc.packages.${system}.default
+        ]
+        ++ krc.devShells.${system}.default.shellPkgs
+        ++ env_ccache.devShells.${system}.default.shellPkgs ;
         packages = self.devShells.${system}.default.shellPkgs;
 
         shellHook = ''
