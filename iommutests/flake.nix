224d4dc737e443d12efcc3c76e9ff60e9d8e23cc
Joel Granados <joel.granados@joelgranados.com>
2025-02-26 15:23:34 +0100

    iommutests: Bring linux headers from nix unstable for iommufd fault queues
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- iommutests/flake.nix
+++ iommutests/flake.nix
@@ -5,13 +5,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixUpkgs.url =  "github:NixOS/nixpkgs/nixos-unstable";
     libvfn.url = "github:Joelgranados/libvfn/cc68647b8a4d95d3cb101e036a5662dbb0f696d5";
     env_shell.url = "github:Joelgranados/nix_envs?dir=env_shell";
   };
 
-  outputs = { self, nixpkgs, libvfn, env_shell, ... }:
+  outputs = { self, nixpkgs, nixUpkgs, libvfn, env_shell, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
+      uPkgs = import nixUpkgs { system = "x86_64-linux"; };
       system = "x86_64-linux";
     in {
       devShells.${system}.default = pkgs.mkShell {
@@ -31,10 +33,12 @@
           git-filter-repo
           clang-tools
           man-pages
+          uPkgs.linuxHeaders
         ];
         hardeningDisable = ["fortify"];
 
         shellHook = ''
+          export C_INCLUDE_PATH="${uPkgs.linuxHeaders}/include:$C_INCLUDE_PATH"
           export _prompt_sorin_prefix="%F{green}(I8S)"
         ''
         + env_shell.devShells.${system}.default.shellHook
