8f389394df4f206a1b296ddd8d4ae311788c9a20
Joel Granados <joel.granados@joelgranados.com>
2025-07-16 13:48:39 +0200

    iommutests: update to latest nix
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- iommutests/flake.nix
+++ iommutests/flake.nix
@@ -4,16 +4,14 @@
   description = "iommutests dev flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixUpkgs.url =  "github:NixOS/nixpkgs/nixos-unstable";
-    libvfn.url = "github:Joelgranados/libvfn/cc68647b8a4d95d3cb101e036a5662dbb0f696d5";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    libvfn.url = "github:Joelgranados/libvfn/d511b650cdc26bc00fbf2ea8cf5684cc952af4e4";
     env_shell.url = "github:Joelgranados/nenvs?dir=env_shell";
   };
 
-  outputs = { self, nixpkgs, nixUpkgs, libvfn, env_shell, ... }:
+  outputs = { self, nixpkgs, libvfn, env_shell, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
-      uPkgs = import nixUpkgs { system = "x86_64-linux"; };
       system = "x86_64-linux";
     in {
       devShells.${system}.default = pkgs.mkShell {
@@ -26,19 +24,19 @@
           pkg-config
           cmake
           clang-tools
-          nodePackages.pyright
+          #nodePackages.pyright
           man-pages
           python311Packages.pytest
           python311Packages.pyudev
           git-filter-repo
           clang-tools
           man-pages
-          uPkgs.linuxHeaders
+          linuxHeaders
         ];
         hardeningDisable = ["fortify"];
 
         shellHook = ''
-          export C_INCLUDE_PATH="${uPkgs.linuxHeaders}/include:$C_INCLUDE_PATH"
+          export C_INCLUDE_PATH="${pkgs.linuxHeaders}/include:$C_INCLUDE_PATH"
           NIX_ENV_SHELL_PROMPT_PREFIX="%F{green}(IOMMUTESTS)"
         ''
         + env_shell.devShells.${system}.default.shellHook
