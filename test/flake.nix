ca1661255aedba861ef826b1a35bb76ded2ecd33
Joel Granados <joel.granados@joelgranados.com>
2025-08-05 11:25:57 +0200

    test: use the shell_env directory instead of the url
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- test/flake.nix
+++ test/flake.nix
@@ -4,10 +4,11 @@
   description = "test flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    env_shell.url = "../env_shell";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, env_shell, ... }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       system = "x86_64-linux";
@@ -16,11 +17,18 @@
         shellPkgs = with pkgs;
         [
           gnumake
+          public-inbox
         ];
         packages = self.devShells.${system}.default.shellPkgs;
         shellHook = ''
           echo "I'm in the shell hook"
-        '';
+          echo $TMPDIR
+          NIX_ENV_SHELL_ZSHRC_PREFIX="
+            alias lei-stage-a='lei q -v -o ~/Mail/fastmail/lei-staging --dedup=mid -t --no-save -I https://lore.kernel.org/all';
+          "
+        ''
+        + env_shell.devShells.${system}.default.shellHook
+        ;
       };
     };
 }
