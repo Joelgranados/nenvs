50ea94ff894eb32b7bcf00c218df38b86b5f3d86
Joel Granados <joel.granados@joelgranados.com>
2025-08-13 15:02:47 +0200

    iommut: Add iommut_base to the deps
    Signed-off-by: Joel Granados <joel.granados@joelgranados.com>

--- iommut/flake.nix
+++ iommut/flake.nix
@@ -7,9 +7,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     libvfn.url = "github:Joelgranados/libvfn/7766ed4d1fd0e2a73e28b686735cb77abe19ff2b";
     env_shell.url = "github:Joelgranados/nenvs?dir=env_shell";
+    iommut_base.url = "github:Joelgranados/nenvs?dir=iommut_base";
   };
 
-  outputs = { self, nixpkgs, libvfn, env_shell, ... }:
+  outputs = { self, nixpkgs, libvfn, env_shell, iommut_base, ... }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
@@ -18,8 +19,10 @@
       system = "x86_64-linux";
     in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
+        packages = [
           libvfn.packages.${system}.default
+          iommut_base.packages.${system}.default
+        ] ++ (with pkgs; [
           meson
           ninja
           git
@@ -37,7 +40,7 @@
           man-pages
           linuxHeaders
           claude-code
-        ];
+        ]);
         hardeningDisable = ["fortify"];
 
         shellHook = ''
